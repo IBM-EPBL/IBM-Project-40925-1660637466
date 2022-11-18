b9c838a563b0f42e382766bd43c94875d85a133d
Laevijerisha <113106484+Laevijerisha@users.noreply.github.com>
2022-11-18 21:53:44 +0530

    Create app.py

--- Project Development Phase/SPRINT 1/source/python/app.py
+++ Project Development Phase/SPRINT 1/source/python/app.py
@@ -1,70 +1,39 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
- 
- 
-app = Flask(__name__)
- 
- 
-app.secret_key = 'your secret key'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'your password'
-app.config['MYSQL_DB'] = 'geeklogin'
- 
-mysql = MySQL(app)
- 
-@app.route('/')
-@app.route('/login', methods =['GET', 'POST'])
-def login():
-    msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
-        account = cursor.fetchone()
-        if account:
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            msg = 'Logged in successfully !'
-            return render_template('index.html', msg = msg)
-        else:
-            msg = 'Incorrect username / password !'
-    return render_template('login.html', msg = msg)
- 
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    return redirect(url_for('login'))
- 
-@app.route('/register', methods =['GET', 'POST'])
-def register():
-    msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-        account = cursor.fetchone()
-        if account:
-            msg = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form !'
-        else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
-            mysql.connection.commit()
-            msg = 'You have successfully registered !'
-    elif request.method == 'POST':
-        msg = 'Please fill out the form !'
-    return render_template('register.html', msg = msg)
-    
+import os
+import numpy as np
+from flask import Flask, request, render_template
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+app=Flask(_name_)
+model=load_model('ECG.h5') 
+@app.route("/") 
+def about():
+  return render_template("about.html") 
+@app.route("/about") 
+def home():
+  return render_template("about.html") 
+@app.route("/info") 
+def information():
+  return render_template("info.html")
+@app.route("/upload")
+def information():
+  return render_template("index6.html")
+@app.route("/predict",methods-["GET", "POST"]) 
+def upload():
+if request.method== 'POST':
+   f-request.files['file'] 
+   basepath os.path.dirname(file")
+   filepath-os.path.join(basepath, "uploads", f.filename)
+   f.save(filepath)
+   ing-image.load_img(filepath, target_size-(64,64)) 
+   x-image.img_to_array(img)
+   x-np.expand_dims(x,axis=0)
+   pred-model.predict_classes (x)
+   print("prediction", pred)
+   index-['Left Bundle Branch Block', 'Normal', 'Premature Atrial Contraction", 'Premature Ventricular Contractions', 'Right Bundle Branch Block', 'Ventricular Fibrillation']
+   result-str(index[pred[0]])
+   return result#resturing the result
+
+return None
+
+if_name_=="_main_":
+     app.run(debug-false)
