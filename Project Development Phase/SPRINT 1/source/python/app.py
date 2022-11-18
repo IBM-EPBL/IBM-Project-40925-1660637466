ecc32224cb0fb96222231e1569192ca65308c331
Laevijerisha <113106484+Laevijerisha@users.noreply.github.com>
2022-11-18 22:00:08 +0530

    Update app.py

--- Project Development Phase/SPRINT 1/source/python/app.py
+++ Project Development Phase/SPRINT 1/source/python/app.py
@@ -15,25 +15,25 @@ def home():
 def information():
   return render_template("info.html")
 @app.route("/upload")
-def information():
+def test():
   return render_template("index6.html")
 @app.route("/predict",methods-["GET", "POST"]) 
 def upload():
 if request.method== 'POST':
-   f-request.files['file'] 
-   basepath os.path.dirname(file")
-   filepath-os.path.join(basepath, "uploads", f.filename)
+   f=request.files['file'] 
+   basepath=os.path.dirname('_file_')
+   filepath=os.path.join(basepath, "uploads", f.filename)
    f.save(filepath)
-   ing-image.load_img(filepath, target_size-(64,64)) 
-   x-image.img_to_array(img)
-   x-np.expand_dims(x,axis=0)
-   pred-model.predict_classes (x)
+   img=image.load_img(filepath, target_size=(64,64)) 
+   x=image.img_to_array(img)
+   x=np.expand_dims(x,axis=0)
+   pred=model.predict_classes (x)
    print("prediction", pred)
-   index-['Left Bundle Branch Block', 'Normal', 'Premature Atrial Contraction", 'Premature Ventricular Contractions', 'Right Bundle Branch Block', 'Ventricular Fibrillation']
-   result-str(index[pred[0]])
-   return result#resturing the result
+   index=['Left Bundle Branch Block', 'Normal', 'Premature Atrial Contraction", 'Premature Ventricular Contractions', 'Right Bundle Branch Block', 'Ventricular Fibrillation']
+   result=str(index[pred[0]])
+   return result
 
 return None
 
 if_name_=="_main_":
-     app.run(debug-false)
+     app.run(debug=false)
